2f8d09b0a96e4831beab255ad097403c9c4f7e9b
Yogi Arif Widodo <yogirenbox33@gmail.com>
2019-09-22 16:24:42 +0800

    Added new def as footer in start

--- dicoding.py
+++ dicoding.py
@@ -5,12 +5,17 @@ from color import ColorPrint as warna
 print("dicodingidn")
 
 
+def footer():
+    warna.print_fail("===============FOOTER===============")
+
+
 def start():
     authorName = "Yogi Arif Widodo  | yogithesymbian"
     authorUrl = "image url(https://avatars2.githubusercontent.com/u/28316296?s=400&u=e22e07736450e363997d6eb3534aa184a51c761c&v=4)"
     print(authorName)
     print(authorUrl)
     warna.print_pass("gitkraken")
+    footer()
 
 
 if __name__ == '__main__':
