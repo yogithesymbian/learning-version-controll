38e2c0184c1f3a4f991061cfc9569a14323a415c
Yogi Arif Widodo <yogirenbox33@gmail.com>
2019-09-22 15:28:40 +0800

    Added variable each String value

--- dicoding.py
+++ dicoding.py
@@ -1,11 +1,14 @@
+__author__ = "yogithesymbian"
 import sys
 
 print("dicodingidn")
 
 
 def start():
-    print("Yogi Arif Widodo  | yogithesymbian")
-    print("image url(https://avatars2.githubusercontent.com/u/28316296?s=400&u=e22e07736450e363997d6eb3534aa184a51c761c&v=4)")
+    authorName = "Yogi Arif Widodo  | yogithesymbian"
+    authorUrl = "image url(https://avatars2.githubusercontent.com/u/28316296?s=400&u=e22e07736450e363997d6eb3534aa184a51c761c&v=4)"
+    print(authorName)
+    print(authorUrl)
 
 
 if __name__ == '__main__':
