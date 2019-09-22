64593c5251f875c48f10a661b88abeddaaa98211
Yogi Arif Widodo <yogirenbox33@gmail.com>
2019-09-22 15:32:08 +0800

    added initial color.py to style the dicoding.py

--- dicoding.py
+++ dicoding.py
@@ -1,5 +1,6 @@
 __author__ = "yogithesymbian"
 import sys
+from color import ColorPrint as warna
 
 print("dicodingidn")
 
@@ -9,6 +10,7 @@ def start():
     authorUrl = "image url(https://avatars2.githubusercontent.com/u/28316296?s=400&u=e22e07736450e363997d6eb3534aa184a51c761c&v=4)"
     print(authorName)
     print(authorUrl)
+    warna.print_pass("gitkraken")
 
 
 if __name__ == '__main__':
