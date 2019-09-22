8d0bbef2592f81ff2974b2af82ab4533ba90fb36
Yogi Arif Widodo <yogirenbox33@gmail.com>
2019-09-22 15:20:54 +0800

    Added basic define to try catch python

--- dicoding.py
+++ dicoding.py
@@ -1,1 +1,15 @@
+import sys
+
 print("dicodingidn")
+
+
+def start():
+    print("hello world")
+
+
+if __name__ == '__main__':
+    try:
+        start()
+    except KeyboardInterrupt as err:
+        print(err)
+        sys.exit(0)
