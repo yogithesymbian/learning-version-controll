213a6abbe4dc05f54b965458a8a6d6ae7b4c599e
Yogi Arif Widodo <yogirenbox33@gmail.com>
2019-09-22 16:56:22 +0800

    shortend prPass Parameter

--- color.py
+++ color.py
@@ -12,8 +12,8 @@ class ColorPrint:
         sys.stderr.write('\x1b[1;31m' + message.strip() + '\x1b[0m' + end)
 
     @staticmethod
-    def print_pass(message, end='\n'):
-        sys.stdout.write('\x1b[1;32m' + message.strip() + '\x1b[0m' + end)
+    def print_pass(msg, end='\n'):
+        sys.stdout.write('\x1b[1;32m' + msg.strip() + '\x1b[0m' + end)
 
     @staticmethod
     def print_warn(message, end='\n'):
